683cceb6b79f31e757fbc88c1b40c67cd0e1262f
Sam <samuel@encodevlabs.com>
2024-08-29 23:25:03 +0000

    chore: Add SVG image for 404 error in ErrorScreen

--- lib/screens/error_screen.dart
+++ lib/screens/error_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:keep/constants/text.dart';
+import 'package:keep/utilities/styles.dart';
 
 class ErrorScreen extends StatelessWidget {
   const ErrorScreen({super.key});
@@ -21,10 +22,7 @@ class ErrorScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                pageNotFound,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
+              buildSVG("error_404.svg"),
               const SizedBox(height: 10),
               FilledButton(
                   onPressed: () => context.go("/"),
