493ebddd38ec0d118dba2308590f3caf5677cfeb
Sam <samuel@encodevlabs.com>
2024-09-08 10:20:59 +0000

    Refactor: Remove unused import in onboard_test.dart

--- test/screens/onboard_test.dart
+++ test/screens/onboard_test.dart
@@ -1,17 +1,10 @@
 import 'package:keep/main.dart';
-import 'package:flutter/material.dart';
 import 'package:keep/constants/text.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:keep/routes/route_navigator.dart';
 
 void main() {
   testWidgets('Navigates to Onboard Page', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    await tester.pumpWidget(
-      MaterialApp.router(
-        routerConfig: router(),
-      ),
-    );
 
     expect(find.text(skip), findsOneWidget);
   });
