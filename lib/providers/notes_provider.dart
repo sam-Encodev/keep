8fa25e5fd083d9ff89d0cc9fe0c75fa00394d0a5
Sam <samuel@encodevlabs.com>
2024-09-11 07:01:49 +0000

    change type from set to list

--- lib/providers/notes_provider.dart
+++ lib/providers/notes_provider.dart
@@ -2,127 +2,125 @@ import 'package:jiffy/jiffy.dart';
 import 'package:keep/models/note.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class NoteNotifier extends Notifier<Set<Note>> {
+class NoteNotifier extends Notifier<List<Note>> {
   @override
-  Set<Note> build() {
-    return {
-      Note(
-          id: 0,
-          title: "Game",
-          description: "I lied",
-          timestamp: "2024-03-02T15:18:29.922343",
-          color: "red"),
-      Note(
-          id: 1,
-          title: "Sleep",
-          description: "I lied",
-          timestamp: Jiffy.parse(DateTime.timestamp().toString())
-              .subtractDuration(const Duration(days: 10, hours: 6))
-              .format(),
-          color: "yellow"),
-      Note(
-          id: 2,
-          title: "Sleep",
-          description: "I lied",
-          timestamp: Jiffy.parse(DateTime.timestamp().toString())
-              .subtractDuration(const Duration(days: 10, hours: 6))
-              .format(),
-          color: "lightGreenAccent"),
-      Note(
-          id: 3,
-          title: "Game",
-          description: "I lied",
-          timestamp: "2024-03-02T15:18:29.922343",
-          color: "white"),
-      Note(
-          id: 4,
-          title: "Sleep",
-          description: "I lied",
-          timestamp: Jiffy.parse(DateTime.timestamp().toString())
-              .subtractDuration(const Duration(days: 10, hours: 6))
-              .format(),
-          color: "blue"),
-      Note(
-          id: 5,
-          title: "Game",
-          description: "I lied",
-          timestamp: "2024-03-02T15:18:29.922343",
-          color: "indigo"),
-      Note(
-          id: 6,
-          title: "Sleep",
-          description: "I lied",
-          timestamp: Jiffy.parse(DateTime.timestamp().toString())
-              .subtractDuration(const Duration(days: 10, hours: 6))
-              .format(),
-          color: "orange"),
-      Note(
-          id: 7,
-          title: "Game",
-          description: "I lied",
-          timestamp: "2024-03-02T15:18:29.922343",
-          color: "green"),
-      Note(
-          id: 8,
-          title: "Sleep",
-          description: "I lied",
-          timestamp: Jiffy.parse(DateTime.timestamp().toString())
-              .subtractDuration(const Duration(days: 10, hours: 6))
-              .format(),
-          color: "lightGreenAccent"),
-      Note(
-          id: 9,
-          title: "Game",
-          description: "I lied",
-          timestamp: "2024-03-02T15:18:29.922343",
-          color: "red"),
-      Note(
-          id: 10,
-          title: "Sleep",
-          description: "I lied",
-          timestamp: Jiffy.parse(DateTime.timestamp().toString())
-              .subtractDuration(const Duration(days: 10, hours: 6))
-              .format(),
-          color: "yellow"),
-      // Note(
-      //     id: 11,
-      //     title: "Game",
-      //     description: "I lied",
-      //     timestamp: "2024-03-02T15:18:29.922343",
-      //     color: "red"),
-      // Note(
-      //     id: 12,
-      //     title: "Sleep",
-      //     description: "I lied",
-      //     timestamp: Jiffy.parse(DateTime.timestamp().toString())
-      //         .subtractDuration(const Duration(days: 10, hours: 6))
-      //         .format(),
-      //     color: "yellow"),
-      // Note(
-      //     id: 13,
-      //     title: "Game",
-      //     description: "I lied",
-      //     timestamp: "2024-03-02T15:18:29.922343",
-      //     color: "red"),
-      // Note(
-      //     id: 14,
-      //     title: "Sleep",
-      //     description: "I lied",
-      //     timestamp: Jiffy.parse(DateTime.timestamp().toString())
-      //         .subtractDuration(const Duration(days: 10, hours: 6))
-      //         .format(),
-      //     color: "yellow"),
-    };
-  }
+  List<Note> build() => [
+        Note(
+            id: 0,
+            title: "Game",
+            description: "I lied",
+            timestamp: "2024-03-02T15:18:29.922343",
+            color: "red"),
+        Note(
+            id: 1,
+            title: "Sleep",
+            description: "I lied",
+            timestamp: Jiffy.parse(DateTime.timestamp().toString())
+                .subtractDuration(const Duration(days: 10, hours: 6))
+                .format(),
+            color: "yellow"),
+        Note(
+            id: 2,
+            title: "Sleep",
+            description: "I lied",
+            timestamp: Jiffy.parse(DateTime.timestamp().toString())
+                .subtractDuration(const Duration(days: 10, hours: 6))
+                .format(),
+            color: "lightGreenAccent"),
+        Note(
+            id: 3,
+            title: "Game",
+            description: "I lied",
+            timestamp: "2024-03-02T15:18:29.922343",
+            color: "white"),
+        Note(
+            id: 4,
+            title: "Sleep",
+            description: "I lied",
+            timestamp: Jiffy.parse(DateTime.timestamp().toString())
+                .subtractDuration(const Duration(days: 10, hours: 6))
+                .format(),
+            color: "blue"),
+        Note(
+            id: 5,
+            title: "Game",
+            description: "I lied",
+            timestamp: "2024-03-02T15:18:29.922343",
+            color: "indigo"),
+        Note(
+            id: 6,
+            title: "Sleep",
+            description: "I lied",
+            timestamp: Jiffy.parse(DateTime.timestamp().toString())
+                .subtractDuration(const Duration(days: 10, hours: 6))
+                .format(),
+            color: "orange"),
+        Note(
+            id: 7,
+            title: "Game",
+            description: "I lied",
+            timestamp: "2024-03-02T15:18:29.922343",
+            color: "green"),
+        Note(
+            id: 8,
+            title: "Sleep",
+            description: "I lied",
+            timestamp: Jiffy.parse(DateTime.timestamp().toString())
+                .subtractDuration(const Duration(days: 10, hours: 6))
+                .format(),
+            color: "lightGreenAccent"),
+        Note(
+            id: 9,
+            title: "Game",
+            description: "I lied",
+            timestamp: "2024-03-02T15:18:29.922343",
+            color: "red"),
+        Note(
+            id: 10,
+            title: "Sleep",
+            description: "I lied",
+            timestamp: Jiffy.parse(DateTime.timestamp().toString())
+                .subtractDuration(const Duration(days: 10, hours: 6))
+                .format(),
+            color: "yellow"),
+        // Note(
+        //     id: 11,
+        //     title: "Game",
+        //     description: "I lied",
+        //     timestamp: "2024-03-02T15:18:29.922343",
+        //     color: "red"),
+        // Note(
+        //     id: 12,
+        //     title: "Sleep",
+        //     description: "I lied",
+        //     timestamp: Jiffy.parse(DateTime.timestamp().toString())
+        //         .subtractDuration(const Duration(days: 10, hours: 6))
+        //         .format(),
+        //     color: "yellow"),
+        // Note(
+        //     id: 13,
+        //     title: "Game",
+        //     description: "I lied",
+        //     timestamp: "2024-03-02T15:18:29.922343",
+        //     color: "red"),
+        // Note(
+        //     id: 14,
+        //     title: "Sleep",
+        //     description: "I lied",
+        //     timestamp: Jiffy.parse(DateTime.timestamp().toString())
+        //         .subtractDuration(const Duration(days: 10, hours: 6))
+        //         .format(),
+        //     color: "yellow"),
+      ];
 
   void addNote(Note note) {
     if (!state.contains(note)) {
-      state = {...state, note};
+      state = [...state, note];
     }
   }
 
   void removeNote(noteID) {
-    state = state.where((p) => p.id != noteID).toSet();
+    state = state.where((p) => p.id != noteID).toList();
 
     // if (state.contains(noteID)) {
     //   state = state.where((p) => p.id != noteID).toSet();
@@ -138,10 +136,10 @@ class NoteNotifier extends Notifier<Set<Note>> {
     oldNote.first.color = note.color;
     oldNote.first.timestamp = note.timestamp;
 
-    state = {...state};
+    state = [...state];
   }
 }
 
-final noteNotifierProvider = NotifierProvider<NoteNotifier, Set<Note>>(() {
+final noteNotifierProvider = NotifierProvider<NoteNotifier, List<Note>>(() {
   return NoteNotifier();
 });
