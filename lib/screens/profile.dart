653c1198c1a340fff9234adf19f4205479c326ef
Sam <samuel@encodevlabs.com>
2024-09-13 23:12:55 +0000

    minor changes

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -15,9 +15,9 @@ class Profile extends ConsumerStatefulWidget {
 class _Profile extends ConsumerState<Profile> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  static const spacing = 18.0;
-  static const labelSize = 13.0;
-  static const labelSpacer = 12.0;
+  static const spacing = 8.0;
+  static const labelSize = 15.0;
+  static const labelSpacer = 11.0;
 
   String? _email;
   String? _lastName;
@@ -40,7 +40,7 @@ class _Profile extends ConsumerState<Profile> {
           automaticallyImplyLeading: false,
           backgroundColor: Colors.black87,
           leading: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(spacing),
             child: IconButton(
               style: iconButtonStyle(),
               padding: EdgeInsets.zero,
@@ -53,7 +53,6 @@ class _Profile extends ConsumerState<Profile> {
               style: TextStyle(color: Colors.white, fontSize: 20)),
         ),
         body: Container(
-          padding: const EdgeInsets.all(10),
           width: double.infinity,
           decoration: const BoxDecoration(
             color: Colors.black87,
@@ -64,154 +63,175 @@ class _Profile extends ConsumerState<Profile> {
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const SizedBox(height: padding),
-                  const Text(firstName,
-                      style:
-                          TextStyle(color: Colors.white, fontSize: labelSize)),
-                  const SizedBox(height: labelSpacer),
-                  TextFormField(
-                    initialValue: user.firstName,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        fillColor: Colors.black12,
-                        filled: true,
-                        enabledBorder: inputBorder(),
-                        focusedBorder: focusBorder(),
-                        errorBorder: inputBorder(),
-                        focusedErrorBorder: errorBorder(),
-                        errorStyle: errorStyle()),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return errorEntry;
-                      }
-
-                      _firstName = value;
-                      return null;
-                    },
+                  const Padding(
+                    padding: EdgeInsets.all(spacing),
+                    child: Text(firstName,
+                        style: TextStyle(
+                            color: Colors.white, fontSize: labelSize)),
                   ),
-                  const SizedBox(height: spacing),
-                  const Text(lastName,
-                      style:
-                          TextStyle(color: Colors.white, fontSize: labelSize)),
-                  const SizedBox(height: labelSpacer),
-                  TextFormField(
-                    initialValue: user.lastName,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      fillColor: Colors.black12,
-                      filled: true,
-                      enabledBorder: inputBorder(),
-                      focusedBorder: focusBorder(),
-                      errorBorder: inputBorder(),
-                      focusedErrorBorder: errorBorder(),
-                      errorStyle: errorStyle(),
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return errorEntry;
-                      }
-
-                      _lastName = value;
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: spacing),
-                  const Text(email,
-                      style:
-                          TextStyle(color: Colors.white, fontSize: labelSize)),
-                  const SizedBox(height: labelSpacer),
-                  TextFormField(
-                    initialValue: user.email,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      fillColor: Colors.black12,
-                      filled: true,
-                      enabledBorder: inputBorder(),
-                      focusedBorder: focusBorder(),
-                      errorBorder: inputBorder(),
-                      focusedErrorBorder: errorBorder(),
-                      errorStyle: errorStyle(),
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return errorEntry;
-                      }
-
-                      _email = value;
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: spacing),
-                  const Text(currentPassword,
-                      style:
-                          TextStyle(color: Colors.white, fontSize: labelSize)),
-                  const SizedBox(height: labelSpacer),
-                  TextFormField(
-                    initialValue: user.password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    obscuringCharacter: "*",
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        fillColor: Colors.black12,
-                        filled: true,
-                        enabledBorder: inputBorder(),
-                        focusedBorder: focusBorder(),
-                        errorBorder: inputBorder(),
-                        focusedErrorBorder: errorBorder(),
-                        errorStyle: errorStyle()),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return errorEntry;
-                      }
-                      _currentPassword = value;
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: spacing),
-                  const Text(newPassword,
-                      style:
-                          TextStyle(color: Colors.white, fontSize: labelSize)),
-                  const SizedBox(height: labelSpacer),
-                  TextFormField(
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    obscuringCharacter: "*",
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        fillColor: Colors.black12,
-                        filled: true,
-                        enabledBorder: inputBorder(),
-                        focusedBorder: focusBorder(),
-                        errorBorder: inputBorder(),
-                        focusedErrorBorder: errorBorder(),
-                        errorStyle: errorStyle()),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return errorEntry;
-                      }
-                      _newPassword = value;
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: spacing),
-                  FilledButton(
-                      style: buttonStyle(),
-                      onPressed: () {
-                        // Validate will return true if the form is valid, or false if
-                        // the form is invalid.
-                        if (_formKey.currentState!.validate()) {
-                          // Process data.
-                        } else {
-                          setState(() {});
+                  Padding(
+                    padding: const EdgeInsets.all(spacing),
+                    child: TextFormField(
+                      initialValue: user.firstName,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          fillColor: Colors.black12,
+                          filled: true,
+                          enabledBorder: inputBorder(),
+                          focusedBorder: focusBorder(),
+                          errorBorder: inputBorder(),
+                          focusedErrorBorder: errorBorder(),
+                          errorStyle: errorStyle()),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return errorEntry;
                         }
+
+                        _firstName = value;
+                        return null;
                       },
-                      child: const Text(updateProfile,
-                          style: TextStyle(
-                              color: Colors.redAccent, fontSize: 20))),
-                  const SizedBox(height: spacing),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(spacing),
+                    child: Text(lastName,
+                        style:
+                            TextStyle(color: Colors.white, fontSize: labelSize)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(spacing),
+                    child: TextFormField(
+                      initialValue: user.lastName,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        fillColor: Colors.black12,
+                        filled: true,
+                        enabledBorder: inputBorder(),
+                        focusedBorder: focusBorder(),
+                        errorBorder: inputBorder(),
+                        focusedErrorBorder: errorBorder(),
+                        errorStyle: errorStyle(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return errorEntry;
+                        }
+
+                        _lastName = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(spacing),
+                    child: Text(email,
+                        style:
+                            TextStyle(color: Colors.white, fontSize: labelSize)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(spacing),
+                    child: TextFormField(
+                      initialValue: user.email,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        fillColor: Colors.black12,
+                        filled: true,
+                        enabledBorder: inputBorder(),
+                        focusedBorder: focusBorder(),
+                        errorBorder: inputBorder(),
+                        focusedErrorBorder: errorBorder(),
+                        errorStyle: errorStyle(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return errorEntry;
+                        }
+
+                        _email = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(spacing),
+                    child: Text(currentPassword,
+                        style:
+                            TextStyle(color: Colors.white, fontSize: labelSize)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(spacing),
+                    child: TextFormField(
+                      initialValue: user.password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      obscuringCharacter: "*",
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          fillColor: Colors.black12,
+                          filled: true,
+                          enabledBorder: inputBorder(),
+                          focusedBorder: focusBorder(),
+                          errorBorder: inputBorder(),
+                          focusedErrorBorder: errorBorder(),
+                          errorStyle: errorStyle()),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return errorEntry;
+                        }
+                        _currentPassword = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(spacing),
+                    child: Text(newPassword,
+                        style:
+                            TextStyle(color: Colors.white, fontSize: labelSize)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(spacing),
+                    child: TextFormField(
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      obscuringCharacter: "*",
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          fillColor: Colors.black12,
+                          filled: true,
+                          enabledBorder: inputBorder(),
+                          focusedBorder: focusBorder(),
+                          errorBorder: inputBorder(),
+                          focusedErrorBorder: errorBorder(),
+                          errorStyle: errorStyle()),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return errorEntry;
+                        }
+                        _newPassword = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(spacing),
+                    child: FilledButton(
+                        style: buttonStyle(),
+                        onPressed: () {
+                          // Validate will return true if the form is valid, or false if
+                          // the form is invalid.
+                          if (_formKey.currentState!.validate()) {
+                            // Process data.
+                          } else {
+                            setState(() {});
+                          }
+                        },
+                        child: const Text(updateProfile,
+                            style: TextStyle(
+                                color: Colors.redAccent, fontSize: 20))),
+                  ),
                 ]),
           ),
         ));
