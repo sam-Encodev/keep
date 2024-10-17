7a7b15671aba0a3dc310855c63943448ec537f38
Sam <samuel@encodevlabs.com>
2024-10-17 16:26:03 +0000

    added splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:keep/widgets/app_start_up.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(
     ProviderScope(
         child: MaterialApp(
--- lib/widgets/app_start_up.dart
+++ lib/widgets/app_start_up.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:keep/widgets/keep.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class AppStartUp extends ConsumerWidget {
+class AppStartUp extends ConsumerStatefulWidget {
   const AppStartUp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Keep();
+  ConsumerState<AppStartUp> createState() => _AppStartUpState();
+}
+
+class _AppStartUpState extends ConsumerState<AppStartUp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Keep();
   }
 }
