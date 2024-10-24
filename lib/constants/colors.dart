10e5c035d6aea4a5e02c969c70531e4072b90930
Sam <samuel@encodevlabs.com>
2024-10-24 13:07:51 +0000

    added color scheme

---
+++ lib/constants/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFFFFFFFF),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFFFD8E6),
+  onPrimaryContainer: Color(0xFF3D0024),
+  secondary: Color(0xFF735761),
+  error: Color(0xFF412A33),
+  onError: Color(0xFF412A33),
+  surface: Color(0xFF412A33),
+  onSurface: Color(0xFF412A33),
+  onSecondary: Color(0xFFFFB0D0),
+);
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF000000),
+  onPrimary: Color(0xFF5C113B),
+  primaryContainer: Color(0xFF792952),
+  onPrimaryContainer: Color(0xFFFFD8E6),
+  secondary: Color(0xFFE1BDCA),
+  onSecondary: Color(0xFF412A33),
+  error: Color(0xFF412A33),
+  onError: Color(0xFF412A33),
+  surface: Color(0xFF412A33),
+  onSurface: Color(0xFF412A33),
+);
