e6b32bc6059cb61b500497d280836b883b6d44e2
Sam <samuel@encodevlabs.com>
2024-10-24 22:19:56 +0000

    added colorScheme

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,27 +2,34 @@ import 'package:flutter/material.dart';
 
 const lightColorScheme = ColorScheme(
   brightness: Brightness.light,
-  primary: Color(0xFFFFFFFF),
-  onPrimary: Color(0xFFFFFFFF),
-  primaryContainer: Color(0xFFFFD8E6),
-  onPrimaryContainer: Color(0xFF3D0024),
-  secondary: Color(0xFF735761),
+  primary: Color(0xDD000000),
   error: Color(0xFF412A33),
   onError: Color(0xFF412A33),
-  surface: Color(0xFF412A33),
   onSurface: Color(0xFF412A33),
-  onSecondary: Color(0xFFFFB0D0),
+
+  outline: Color(0x1F000000),
+  outlineVariant: Color(0x9A6F6F6F),
+
+  onPrimary: Color(0xDD000000),
+  secondary: Color(0xDD000000),
+  onSecondary: Color(0xFF757575),
+  surface: Color(0xFFFFFFFF),
+  surfaceContainer: Color(0x1F000000),
 );
+
 const darkColorScheme = ColorScheme(
   brightness: Brightness.dark,
-  primary: Color(0xFF000000),
-  onPrimary: Color(0xFF5C113B),
-  primaryContainer: Color(0xFF792952),
-  onPrimaryContainer: Color(0xFFFFD8E6),
-  secondary: Color(0xFFE1BDCA),
-  onSecondary: Color(0xFF412A33),
+  primary: Color(0xFFFFFFFF),
   error: Color(0xFF412A33),
   onError: Color(0xFF412A33),
-  surface: Color(0xFF412A33),
   onSurface: Color(0xFF412A33),
+
+  outline: Color(0x3CFFFFFF),
+  outlineVariant: Color(0xC66F6F6F),
+
+  onPrimary: Color(0xDD000000),
+  secondary: Color(0xFFFFFFFF),
+  onSecondary: Color(0xFF757575),
+  surface: Color(0xDD000000),
+  surfaceContainer: Color(0xDD000000),
 );
