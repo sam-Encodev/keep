8c6f29057bd79b530c21bd30525e56ad36f15665
Sam <samuel@encodevlabs.com>
2024-10-24 22:43:06 +0000

    added slidable actions

---
+++ lib/components/slidable_actions.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:keep/utilities/date.dart';
+import 'package:go_router/go_router.dart';
+import 'package:keep/utilities/styles.dart';
+import 'package:keep/routes/route_names.dart';
+import 'package:keep/components/snack_bar.dart';
+import 'package:keep/utilities/switch_color.dart';
+import 'package:keep/providers/notes_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class SlidableActions extends ConsumerWidget {
+  final dynamic index;
+  final dynamic notes;
+
+  const SlidableActions({super.key, this.index, this.notes});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Slidable(
+      key: ValueKey(index),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          const SizedBox(width: spacing),
+          SlidableAction(
+            label: 'Edit',
+            icon: Icons.edit,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            foregroundColor: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(10),
+            padding: const EdgeInsets.symmetric(horizontal: padding),
+            onPressed: (_) => {
+              context.push(
+                RouteNames.editNote(notes[index].id),
+              )
+            },
+          ),
+          const SizedBox(width: spacing),
+          SlidableAction(
+            icon: Icons.delete,
+            label: 'Delete',
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            foregroundColor: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(10),
+            padding: const EdgeInsets.symmetric(horizontal: padding),
+            onPressed: (_) => {
+              ref
+                  .read(noteNotifierProvider.notifier)
+                  .removeNote(notes[index].id),
+              snackBar(context, message: "${notes[index].title} deleted")
+            },
+          ),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            color: SwitchColor.switchColor(notes[index].color),
+            borderRadius: BorderRadius.circular(10)),
+        child: ListTile(
+          onTap: () => context.push(RouteNames.viewNote(notes[index].id)),
+          title: Text(
+            notes[index].title,
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+                fontWeight: FontWeight.bold,
+                fontSize: 23),
+          ),
+          subtitle: Text(
+            dateFormatter(notes[index].timestamp),
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/notes/list_notes.dart
+++ lib/screens/notes/list_notes.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:keep/utilities/date.dart';
 import 'package:keep/utilities/styles.dart';
-import 'package:keep/routes/route_names.dart';
 import 'package:keep/screens/empty_state.dart';
-import 'package:keep/components/snack_bar.dart';
-import 'package:keep/utilities/switch_color.dart';
 import 'package:keep/providers/notes_provider.dart';
+import 'package:keep/components/slidable_actions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
@@ -25,66 +21,7 @@ class ListNotes extends ConsumerWidget {
         : ListView.separated(
             itemCount: getNotes.length.toInt(),
             itemBuilder: (BuildContext context, int index) {
-              return Slidable(
-                key: ValueKey(index),
-                endActionPane: ActionPane(
-                  motion: const ScrollMotion(),
-                  children: [
-                    const SizedBox(width: spacing),
-                    SlidableAction(
-                      label: 'Edit',
-                      icon: Icons.edit,
-                      backgroundColor: Theme.of(context).colorScheme.surface,
-                      foregroundColor: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(10),
-                      padding: const EdgeInsets.symmetric(horizontal: padding),
-                      onPressed: (_) => {
-                        context.push(
-                          RouteNames.editNote(notes[index].id),
-                        )
-                      },
-                    ),
-                    const SizedBox(width: spacing),
-                    SlidableAction(
-                      icon: Icons.delete,
-                      label: 'Delete',
-                      backgroundColor: Theme.of(context).colorScheme.surface,
-                      foregroundColor:     Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(10),
-                      padding: const EdgeInsets.symmetric(horizontal: padding),
-                      onPressed: (_) => {
-                        ref
-                            .read(noteNotifierProvider.notifier)
-                            .removeNote(notes[index].id),
-                        snackBar(context,
-                            message: "${notes[index].title} deleted")
-                      },
-                    ),
-                  ],
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: SwitchColor.switchColor(notes[index].color),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: ListTile(
-                    onTap: () =>
-                        context.push(RouteNames.viewNote(notes[index].id)),
-                    title: Text(
-                      notes[index].title,
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 23),
-                    ),
-                    subtitle: Text(
-                      dateFormatter(notes[index].timestamp),
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                          fontSize: 13),
-                    ),
-                  ),
-                ),
-              );
+              return SlidableActions(index: index, notes: notes);
             },
             separatorBuilder: (BuildContext context, int index) => Divider(
               height: spacing,
