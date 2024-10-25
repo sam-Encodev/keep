ba1591d1fe01b132131e094f76d31599dbf6a6b5
Sam <samuel@encodevlabs.com>
2024-10-25 19:11:35 +0000

    added theme support

--- lib/components/switch_icon.dart
+++ lib/components/switch_icon.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:keep/utilities/styles.dart';
 
-switchIcon(message) {
+switchIcon(context, message) {
   if (message.toString().contains("deleted")) {
     return Icon(Icons.check_circle_outline_outlined,
-        size: standardIcon, color: Colors.green);
+        size: standardIcon,
+        color: Theme.of(context).colorScheme.onPrimaryFixed);
   }
   return Icon(Icons.error_outline_outlined,
-      size: standardIcon, color: Colors.red);
+      size: standardIcon, color: Theme.of(context).colorScheme.tertiary);
 }
