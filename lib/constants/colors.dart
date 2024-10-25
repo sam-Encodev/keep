023e7ffee97694663dbeba7d815dfa1b3f92f005
Sam <samuel@encodevlabs.com>
2024-10-25 17:36:37 +0000

    added more colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
 
 const lightColorScheme = ColorScheme(
-  brightness: Brightness.light,
-  primary: Color(0xDD000000),
-  error: Color(0xFF412A33),
-  onError: Color(0xFF412A33),
-  onSurface: Color(0xFF412A33),
+    brightness: Brightness.light,
+    primary: Color(0xDD000000),
+    error: Color(0xFF412A33),
+    onError: Color(0xFF412A33),
+    onSurface: Color(0xFF412A33),
+    outline: Color(0x1F000000),
+    outlineVariant: Color(0x9A6F6F6F),
+    onPrimary: Color(0xDD000000),
+    secondary: Color(0xDD000000),
+    onSecondary: Color(0xFF757575),
+    surface: Color(0xFFFFFFFF),
+    surfaceContainer: Color(0x1F000000),
 
-  outline: Color(0x1F000000),
-  outlineVariant: Color(0x9A6F6F6F),
-
-  onPrimary: Color(0xDD000000),
-  secondary: Color(0xDD000000),
-  onSecondary: Color(0xFF757575),
-  surface: Color(0xFFFFFFFF),
-  surfaceContainer: Color(0x1F000000),
-);
+    tertiary: Color(0xFFF44336),
+    onTertiary: Color(0xFFFFFFFF),
+    
+    primaryFixed: Color(0xFFFFFFFF),
+    );
 
 const darkColorScheme = ColorScheme(
-  brightness: Brightness.dark,
-  primary: Color(0xFFFFFFFF),
-  error: Color(0xFF412A33),
-  onError: Color(0xFF412A33),
-  onSurface: Color(0xFF412A33),
+    brightness: Brightness.dark,
+    primary: Color(0xFFFFFFFF),
+    error: Color(0xFF412A33),
+    onError: Color(0xFF412A33),
+    onSurface: Color(0xFF412A33),
+    outline: Color(0x3CFFFFFF),
+    outlineVariant: Color(0xC66F6F6F),
+    onPrimary: Color(0xDD000000),
+    secondary: Color(0xFFFFFFFF),
+    onSecondary: Color(0xFF757575),
+    surface: Color(0xDD000000),
+    surfaceContainer: Color(0xDD000000),
 
-  outline: Color(0x3CFFFFFF),
-  outlineVariant: Color(0xC66F6F6F),
 
-  onPrimary: Color(0xDD000000),
-  secondary: Color(0xFFFFFFFF),
-  onSecondary: Color(0xFF757575),
-  surface: Color(0xDD000000),
-  surfaceContainer: Color(0xDD000000),
-);
+    tertiary: Color.fromARGB(255, 244, 54, 54),
+    onTertiary: Color(0xFFFFFFFF),
+    
+    primaryFixed: Color(0xFFFFFFFF),
+    );
