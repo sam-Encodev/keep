b120447ee746bcc4723f42effae7f0a4774fdd94
Sam <samuel@encodevlabs.com>
2024-10-26 07:44:00 +0000

    updated icon switch

--- lib/components/switch_icon.dart
+++ lib/components/switch_icon.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:keep/utilities/styles.dart';
 
 switchIcon(context, message) {
-  if (message.toString().contains("deleted")) {
-    return Icon(Icons.check_circle_outline_outlined,
-        size: standardIcon,
-        color: Theme.of(context).colorScheme.onPrimaryFixed);
+  switch (message) {
+    case "Note deleted":
+    case "Update successful":
+      return Icon(Icons.check_circle_outline_outlined,
+          size: standardIcon,
+          color: Theme.of(context).colorScheme.onPrimaryFixed);
+
+    case "Login failed":
+    case "Email already exists":
+    case "Update failed":
+      return Icon(Icons.error_outline_outlined,
+          size: standardIcon, color: Theme.of(context).colorScheme.tertiary);
   }
-  return Icon(Icons.error_outline_outlined,
-      size: standardIcon, color: Theme.of(context).colorScheme.tertiary);
 }
