9ce7e57e412a1030c83d30d2747110d5425e335d
Sam <samuel@encodevlabs.com>
2024-10-31 16:36:07 +0000

    added sharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:keep/widgets/app_start_up.dart';
+import 'package:keep/providers/lean_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  final sharedPreferences = await SharedPreferences.getInstance();
 
   runApp(ProviderScope(
+    overrides: [
+      sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+    ],
     child: AppStartUp(),
   ));
 }
