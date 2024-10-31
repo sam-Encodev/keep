5f55eba8a8b3493af62c9c7b4e98c03a974f2008
Sam <samuel@encodevlabs.com>
2024-10-31 16:41:22 +0000

    updated routeNames

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,17 +1,17 @@
 class RouteNames {
   static const home = "/notes";
-  static const about = "/about";
   static const login = "/login";
+  static const about = "/about";
   static const signup = "/signup";
   static const onboard = "/onboard";
   static const settings = "/settings";
 
-  static const search = "search";
-  static const newN = "new";
+  static const add = "add";
   static const view = ":noteId";
+  static const search = "search";
   static const edit = "edit/:noteId";
 
-  static const newNote = "/notes/$newN";
+  static const newNote = "/notes/$add";
   static const searchNote = "/notes/$search";
   static viewNote(noteId) => "/notes/$noteId";
   static profile(userId) => "/profile/$userId";
