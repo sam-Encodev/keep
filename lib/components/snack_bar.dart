317f1cebfe976dadef39ee88532b9a7c591120cd
Sam <samuel@encodevlabs.com>
2024-11-04 22:14:24 +0000

    following snackbar guidelines

--- lib/components/snack_bar.dart
+++ lib/components/snack_bar.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:keep/utilities/styles.dart';
-import 'package:keep/components/switch_icon.dart';
 
 snackBar(context, {message}) {
   return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          switchIcon(context, message),
-          SizedBox(width: spacing),
           Text(
             message,
             style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
